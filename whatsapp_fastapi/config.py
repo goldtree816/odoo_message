f0d1f804dd37dc773994a186448f8aa27cd8ffbc
prashanta khatri <tokikin21@gmail.com>
2026-07-12 10:58:10 +0545

    Update config.py

--- whatsapp_fastapi/config.py
+++ whatsapp_fastapi/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ODOO_URL = os.getenv("ODOO_URL", "http://localhost:8069")
-ODOO_DB = os.getenv("ODOO_DB", "hospital_db")
+ODOO_DB = os.getenv("ODOO_DB", "hospital")
 ODOO_USER = os.getenv("ODOO_USER", "admin")
-ODOO_PASSWORD = os.getenv("ODOO_PASSWORD", "admin123")
+ODOO_PASSWORD = os.getenv("ODOO_PASSWORD", "123")
 
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID", "")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN", "")
