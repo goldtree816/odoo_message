268281f00ba68f45dc2289f66ac0a90a007d63ab
prashanta khatri <tokikin21@gmail.com>
2026-07-28 08:52:25 +0545

    Update purchased_number.py

--- custom_addons/whatsapp_dashboard/models/purchased_number.py
+++ custom_addons/whatsapp_dashboard/models/purchased_number.py
@@ -1,8 +1,12 @@
 from odoo import models, fields
 
+
 class PurchasedNumber(models.Model):
+    # Stores the WhatsApp-capable (Twilio) phone numbers purchased/activated
+    # for this Odoo database.
     _name = 'whatsapp.purchased_number'
     _description = 'Purchased WhatsApp Numbers'
+
 
     number = fields.Char('Phone Number', required=True)
     sid = fields.Char('Twilio SID')
@@ -13,3 +17,13 @@ class PurchasedNumber(models.Model):
         ('failed', 'Failed'),
     ], default='pending')
     purchase_date = fields.Datetime(default=fields.Datetime.now)
+
+    is_sending_number = fields.Boolean(
+        'Active for Sending',
+        default=False,
+        help='Only one number can be active for sending at a time.',
+    )
+
+    # NEW: Link to the subaccount that owns this number
+    subaccount_id = fields.Many2one('whatsapp.subaccount', string='Subaccount')
+
